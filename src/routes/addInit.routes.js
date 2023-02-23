d29cce145afc3e923d817076bf0c7e1f5df19379
Sergey Shirobokov <49881602+Hronius@users.noreply.github.com>
2023-02-23 14:53:23 +0300

    html

--- src/routes/addInit.routes.js
+++ src/routes/addInit.routes.js
@@ -2,15 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-<<<<<<< HEAD
-const renderAddInit = require('../controllers/AddInitController');
 
-router.get('/addInit', renderAddInit).post('/addInit', renderAddInit);
-=======
 const { renderAddInitController, addInitFormController } = require('../controllers/AddInitController');
 
 router.get('/addInit', renderAddInitController);
 router.post('/addInit', addInitFormController);
->>>>>>> 71fa6f5cddb31d43b272be6c70385dbf07d4b95b
 
 module.exports = router;
