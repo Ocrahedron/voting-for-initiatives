b538bc40d916d43f68cb77635cee0262fe40593b
Sergey Shirobokov <49881602+Hronius@users.noreply.github.com>
2023-02-23 19:06:02 +0300

    html

--- src/views/auth/LoginPage.jsx
+++ src/views/auth/LoginPage.jsx
@@ -4,14 +4,66 @@ const Layout = require('../Layout');
 module.exports = function LoginPage() {
   return (
     <Layout>
-    
-       <main className="main">
+      <div className="containerAuthBody">
+        <main className="mainAut">
+          <div className="containerAuth">
+            <div className="commonBody">
+              <h3 className="authH3">Вход в аккаунт</h3>
+              <form name="signinForm" action="/auth/signin" method="post">
+                <div className="authInput">
+                  <input
+                    name="name"
+                    className="inputexample"
+                    type="text"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Логин"
+                    required
+                  ></input>
+                </div>
+                <div className="authInput">
+                  <input
+                    name="password"
+                    className="inputexample"
+                    type="password"
+                    id="exampleInputPassword1"
+                    placeholder="Пароль"
+                    required
+                  ></input>
+                </div>
+                {/* <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <span className='spanText'>или</span>
+                </div> */}
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    margin: "15px 0",
+                  }}
+                >
+                  <a href="/auth/google">
+                    <img src="/img/google-logo.png"></img>
+                  </a>
+                </div>
+                <button type="submit" className="btnAuth">
+                  Вход
+                </button>
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
+      {/* <main className="main">
      <div className="container">
      <section className="wrapper">
      <div className="heading">
               <h1 className="text text-large">Авторизируйтесь</h1>
-            </div>
-     
+            </div>     
         <form>
           <div className="form-outline mb-4">
           <label className="form-label" htmlFor="form2Example1">
@@ -22,8 +74,7 @@ module.exports = function LoginPage() {
               id="form2Example1"
               className="form-control"
               name="email"
-            />
-           
+            />           
           </div>
           <div className="form-outline mb-4">
           <label className="form-label" htmlFor="form2Example2">
@@ -57,7 +108,7 @@ module.exports = function LoginPage() {
      
       </section>
       </div>
-      </main>
+      </main> */}
     </Layout>
   );
 };
